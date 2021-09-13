635cb5b397a17362195e0cb7f82556775f15fc5d
kgyeongmi21 <70732096+kgyeongmi21@users.noreply.github.com>
2021-09-13 17:43:17 +0900

    최종 과제 완성

--- app/src/main/java/com/example/helloandriod/MainActivity.kt
+++ app/src/main/java/com/example/helloandriod/MainActivity.kt
@@ -1,16 +1,25 @@
-package com.example.helloandriod
+package com.example.helloandriod;
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView
+import android.widget.Toast;
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var imageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageView = findViewById<ImageView>(R.id.imageView)
     }
 
-    fun onClick(view: android.view.View) {
-        Toast.makeText(this, "안녕하세요 버튼 클릭입니다", Toast.LENGTH_LONG).show()
+    fun onClick(view: View) {
+        imageView.setImageResource(R.drawable.mountain)
+
+        Toast.makeText(this, "이미지를 보여줍니다", Toast.LENGTH_LONG).show()
     }
 }
